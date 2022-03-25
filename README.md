557e6dbf26601c9cc78182eac1e73e7a16595a75
Khai Truong <kminht@gmail.com>
2022-03-25 17:47:04 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Work Day Scheduler Starter Code
 
+-Work day scheduler project scope
+    -User will be presented with a daily planner
+    -Planner will be display the current time and date
+    -Planner will display a list of text area's with the work hours displayed on the left
+    -Planner will indicate the time of the task if its in the present , past, or future
+    -User will be able to save the task and should be reload.
 
 
 ## Criteria
@@ -19,3 +25,6 @@ WHEN I refresh the page
 THEN the saved events persist
 
 ````
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $("#currentDay").text(moment().format("LLLL"));
+$(document)
 
 var time = [
   "0900",
@@ -13,11 +14,14 @@ var time = [
 ];
 
 function loadTask() {
+
+
+
   time.forEach((hour) => {
     $(".container").append(`
-              <div class="row time-block">
+              <div class="row time-block" id="time-block">
                   <div class="col-md-1 hour">${hour}hrs</div>
-                  <textarea class="col-md-10 hour-block" id="${hour}"></textarea>
+                  <textarea class="col-md-10 hour-block description" ></textarea>
                   <button class="saveBtn col-md-1" id='saveBtn'><span class="oi oi-lock-locked display-5"></span></button>
               </div>
           `);
@@ -32,21 +36,41 @@ function createTask() {
 
   $(".time-block").each(function () {
     var hourId = $(this).find(".hour").text().split("hrs")[0];
+    $(this).find(".saveBtn").attr("id",hourId);
     console.log(hourId);
     if (currentTime < hourId){
         console.log("this time is future " + hourId);
-        $(this).find(".hour-block").addClass("future");
+        $(this).find(".hour-block").addClass("future").attr("id", hourId);
+
     }
     else if (currentTime > hourId){
         console.log("this time is passed " + hourId)
-        $(this).find(".hour-block").addClass("past");
+        $(this).find(".hour-block").addClass("past").attr("id", hourId);
     }
     else{
         console.log("this time is present " + hourId)
-        $(this).find(".hour-block").addClass("present");
+        $(this).find(".hour-block").addClass("present").attr("id", hourId);
     }
 
   });
 }
+
+$(".container").on("click", "button", function(){
+
+    console.log("save btn clicked" + event.target);
+
+    var text = $(this).siblings('.description').val();
+
+    var time = $(this).find('.hour').text().trim();
+    
+
+    console.log("saved text " +text);
+    console.log("saved time " +time);
+
+    localStorage.setItem(time, text);
+
+
+})
+
 
 createTask();
