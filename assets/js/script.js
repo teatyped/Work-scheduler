41302e41ada342b1f06fbb99635886665607bbd3
Khai Truong <kminht@gmail.com>
2022-03-25 14:50:46 -0400

    creat task made, will load and check hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,49 @@
 $("#currentDay").text(moment().format("LLLL"));
 
-var time = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
+var time = [
+  "0900",
+  "1000",
+  "1100",
+  "1200",
+  "1300",
+  "1400",
+  "1500",
+  "1600",
+  "1700",
+];
 
-time.forEach(hour => {
-    
-    $('.container').append(`
-        <div class="row">
-            <div class="col-md-1 hour">${hour}</div>
-            <textarea class="col-md-10 present"></textarea>
-            <div class="col-md-1 saveBtn"></div>
-        </div>
-    `)
-});
+function loadTask() {
+  time.forEach((hour) => {
+    $(".container").append(`
+              <div class="row time-block">
+                  <div class="col-md-1 hour">${hour}hrs</div>
+                  <textarea class="col-md-10" id="${hour}"></textarea>
+                  <button class="saveBtn col-md-1" id='saveBtn'><span class="oi oi-lock-locked display-5"></span></button>
+              </div>
+          `);
+  });
+}
+
+function createTask() {
+  loadTask();
+
+  var currentTime = moment().format("HH" + "00");
+  console.log(currentTime); // testing
+
+  $(".time-block").each(function () {
+    var hourId = $(this).find(".hour").text().split("hrs")[0];
+    console.log(hourId);
+    if (currentTime < hourId){
+        console.log("this time is future " + hourId)
+    }
+    else if (currentTime > hourId){
+        console.log("this time is passed " + hourId)
+    }
+    else{
+        console.log("this time is present " + hourId)
+    }
+
+  });
+}
+
+createTask();
